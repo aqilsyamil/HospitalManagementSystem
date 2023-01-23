4cf469a394e4a74dd754fa6da93e8e246706ef49
aqilsyamil <78575059+aqilsyamil@users.noreply.github.com>
2023-01-24 00:25:24 +0800

    Add files via upload

---
+++ HospitalManagementSystem/src/main/java/com/hms/controller/AppController.java
@@ -0,0 +1,59 @@
+package com.hms.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.hms.model.User;
+import com.hms.model.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+ 
+@Controller
+public class AppController {
+ 
+    @Autowired
+    private UserRepository userRepo;
+     
+    @GetMapping("/")
+    public String viewHomePage() {
+//    	ModelAndView MV = new ModelAndView();
+//    	MV.setViewName("landing");
+        return "landing";
+    }
+    
+    @GetMapping("/account")
+    public String viewAccountPage() {
+        return "signup_or_login";
+    } 
+    
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model) {
+        model.addAttribute("user", new User());
+         
+        return "signup_form";
+    }
+    
+    @PostMapping("/process_register")
+    public String processRegister(User user) {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+         
+        userRepo.save(user);
+         
+        return "register_success";
+    }
+}
---
+++ HospitalManagementSystem/src/main/java/com/hms/controller/LoginController.java
@@ -0,0 +1,40 @@
+package com.hms.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import com.hms.service.LoginService;
+
+@Controller
+@SessionAttributes("name")
+public class LoginController {
+
+    @Autowired
+    LoginService service;
+
+    @GetMapping("/login")
+    public String showLoginPage(ModelMap model){
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String showWelcomePage(ModelMap model, @RequestParam String name, @RequestParam String password){
+
+        boolean isValidUser = service.validateUser(name, password);
+
+        if (!isValidUser) {
+            model.put("errorMessage", "Invalid Credentials");
+            return "login";
+        }
+
+        model.put("name", name);
+        model.put("password", password);
+
+        return "welcome";
+    }
+
+}
